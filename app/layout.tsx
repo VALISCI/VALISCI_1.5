30f103593492f26832e633ff26927a0cb3746666
VALISCI <jonathanrevere@gmail.com>
2025-10-23 00:03:26 -0400

    Fix layout.tsx (remove diff markers) and render Header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,19 @@
-﻿- export default function RootLayout({ children }: { children: React.ReactNode }) {
-+ import Header from "./Header";
-+ export default function RootLayout({ children }: { children: React.ReactNode }) {
-   return (
-     <html lang="en">
-       <body>
--        {/* header lived in page.tsx before */}
-+        <Header />
-         {children}
-       </body>
-     </html>
-   );
- }
+﻿// app/layout.tsx
+import "./globals.css";
+import Header from "./Header";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <Header />
+        {children}
+      </body>
+    </html>
+  );
+}
+
